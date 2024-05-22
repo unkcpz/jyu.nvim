0a1c9de261adcb099ddbfd543d0bc62f1188f996
Jusong Yu <jusong.yeu@gmail.com>
2024-05-22 16:14:47 +0200

    rustacean keymappings to run and debug

--- lua/unkcpz/plugins/rustacean.lua
+++ lua/unkcpz/plugins/rustacean.lua
@@ -2,4 +2,12 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^4', -- Recommended
   lazy = false, -- This plugin is already lazy
+  ft = { "rust" },
+  config = function()
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>crr", "<cmd>RustLsp runnables<CR>", { desc = "RustLsp runnables" }) -- run rust
+    keymap.set("n", "<leader>crf", "<cmd>RustFmt<CR>", { desc = "Rust formatting" }) -- run rust formatting
+  end
 }
