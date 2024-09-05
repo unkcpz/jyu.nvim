ea99ff94cd9f7360fe17e01f90f1fc841f6d0b9c
Jusong Yu <jusong.yeu@gmail.com>
2024-09-05 22:50:57 +0200

    Using vim/slime to send code to REPL

---
+++ lua/unkcpz/plugins/slime.lua
@@ -0,0 +1,16 @@
+return {
+    -- slime (REPL integration)
+    -- https://github.com/LazyVim/LazyVim/discussions/234 
+    {
+        "jpalardy/vim-slime",
+        keys = {
+            { "<leader>rc", "<cmd>SlimeConfig<cr>",               desc = "Slime Config" },
+            { "<leader>rr", "<Plug>SlimeSendCell<BAR>/^# %%<CR>", desc = "Slime Send Cell" },
+        },
+        config = function()
+            vim.g.slime_target = "tmux"
+            vim.g.slime_cell_delimiter = "# %%"
+            vim.g.slime_bracketed_paste = 1
+        end,
+    },
+}
