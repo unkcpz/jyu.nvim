1816750721072575cbf5cc08281ae45d546c377b
Jusong Yu <jusong.yeu@gmail.com>
2024-11-14 00:18:32 +0100

    Use naive harpoon2 without telescope
    Now I can delete with dd

--- lua/unkcpz/plugins/harpoon.lua
+++ lua/unkcpz/plugins/harpoon.lua
@@ -16,33 +16,12 @@ return {
         -- Toggle previous & next buffers stored within Harpoon list
         keymap.set('n', '<leader>gp', function()
             harpoon:list():prev()
-        end, {desc = "previous harpoon buf"})
+        end, { desc = 'previous harpoon buf' })
         keymap.set('n', '<leader>gn', function()
             harpoon:list():next()
-        end, {desc = "next harpoon buf"})
-
-        -- basic telescope configuration
-        local conf = require('telescope.config').values
-        local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
-            end
-
-            require('telescope.pickers')
-                .new({}, {
-                    prompt_title = 'Harpoon',
-                    finder = require('telescope.finders').new_table {
-                        results = file_paths,
-                    },
-                    previewer = conf.file_previewer {},
-                    sorter = conf.generic_sorter {},
-                })
-                :find()
-        end
-
+        end, { desc = 'next harpoon buf' })
         keymap.set('n', '<C-e>', function()
-            toggle_telescope(harpoon:list())
-        end, { desc = 'Open harpoon window' })
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
     end,
 }
