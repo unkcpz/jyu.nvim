776765a6d26a0278a761f9f334326b2d6646f124
Jusong Yu <jusong.yeu@gmail.com>
2025-01-01 02:23:19 +0100

    Resolve conflict between suround.nvim and leap.nvim by zurround

--- lua/unkcpz/plugins/surround.lua
+++ lua/unkcpz/plugins/surround.lua
@@ -2,5 +2,21 @@ return {
     'kylechui/nvim-surround',
     event = { 'BufReadPre', 'BufNewFile' },
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
-    config = true,
+    config = function()
+        local surround = require 'nvim-surround'
+        surround.setup {
+            keymaps = {
+                insert = '<C-g>z',
+                insert_line = 'gC-ggZ',
+                normal = 'gz',
+                normal_cur = 'gZ',
+                normal_line = 'gzgz',
+                normal_cur_line = 'gZgZ',
+                visual = 'gz',
+                visual_line = 'gZ',
+                delete = 'gzd',
+                change = 'gzr',
+            },
+        }
+    end,
 }
