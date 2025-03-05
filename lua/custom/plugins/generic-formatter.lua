3d24f204800363ce242c518e1ecd8ace0e3556d5
Jusong Yu <jusong.yeu@gmail.com>
2025-03-05 23:48:25 +0100

    Select in visual mode and formatter, add html formatter to eml

--- lua/custom/plugins/generic-formatter.lua
+++ lua/custom/plugins/generic-formatter.lua
@@ -28,13 +28,24 @@ return {
                 -- Use "yamlfmt" as primary
                 yaml = { 'yamlfmt' },
                 html = { 'htmlbeautifier' },
+                eml = { 'htmlbeautifier' },
             },
         }
+        -- This in order to make html formatter works for .eml
+        vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+            pattern = '*.eml',
+            command = 'set filetype=html',
+        })
 
         local keymap = vim.keymap -- for conciseness
 
         keymap.set('n', '<leader>pf', function()
             require('conform').format { bufnr = vim.api.nvim_get_current_buf() }
         end, { noremap = true, silent = true, desc = 'Format buffer with Conform' })
+
+        -- select in visual mode and format
+        keymap.set('v', '<leader>pf', function()
+            vim.lsp.buf.format { range = { ['start'] = vim.api.nvim_buf_get_mark(0, '<'), ['end'] = vim.api.nvim_buf_get_mark(0, '>') } }
+        end, { desc = 'Format Selection' })
     end,
 }
