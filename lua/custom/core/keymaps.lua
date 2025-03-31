0a605646ad6e9fad3fae7fb60b6c1e218d034d28
Jusong Yu <jusong.yeu@gmail.com>
2025-03-31 18:40:58 +0200

    window resize

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -19,6 +19,11 @@ keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' }) 
 keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' }) -- make split windows equal width & height
 keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' }) -- close current split window
 
+keymap.set('n', '<leader>s+', ':resize +5<CR>', { desc = "Increase window height" })
+keymap.set('n', '<leader>s-', ':resize -5<CR>', { desc = "Decrease window height" })
+keymap.set('n', '<leader>s>', ':vertical resize +5<CR>', { desc = "Increase window width" })
+keymap.set('n', '<leader>s<', ':vertical resize -5<CR>', { desc = "Decrease window width" })
+
 keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
 keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
 keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
