926d40f1c1820d00a6e88284d6af1ab02896a45a
Jusong Yu <jusong.yeu@gmail.com>
2025-07-23 11:33:35 +0200

    remove jk as ESC

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -2,8 +2,6 @@ vim.g.mapleader = ' '
 
 local keymap = vim.keymap -- for conciseness
 
-keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
-
 keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
 
 -- delete single character without copying into register
