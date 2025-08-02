7ef3db8e26e2f8059c9953759a1021ab8834d04d
Jusong Yu <jusong.yeu@gmail.com>
2025-08-02 18:23:23 +0200

    customize leap preview highlight

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,6 +1,13 @@
 return {
     'ggandor/leap.nvim',
     config = function()
-        require('leap').create_default_mappings()
+        local leap = require 'leap'
+        leap.create_default_mappings()
+
+        -- Disable auto-jumping to the first match
+        leap.opts.safe_labels = {}
+
+        -- Greying out the search area
+        vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
     end,
 }
